529f028d19c8414cb4ad32e69be040502e5976f8
Brian Ma <spo2.spire.club@gmail.com>
2018-06-26 11:33:23 +0800

    setup of webpack, babel, react, reactDOM

---
+++ app1/index.js
@@ -0,0 +1,8 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var App1 = require('./components/App1');
+
+ReactDOM.render(
+    <App1 />,
+    document.getElementById('app')
+);
